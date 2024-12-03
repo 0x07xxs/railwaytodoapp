fae3711976ae28ba96538c6e4f1f23f3b8551e04
Exif <lacednyquil@gmail.com>
2024-12-02 16:34:07 -0800

    sdgsdg

--- railwaybackend/src/main.py
+++ railwaybackend/src/main.py
@@ -11,7 +11,10 @@ ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://127.0.0.1:3000").split(",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontend-production-fc16.up.railway.app/"],  # Update this with your frontend URL in production
+    allow_origins=[
+        "https://frontend-production-fc16.up.railway.app/",
+        "http://localhost:3000"
+        ],  # Update this with your frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
