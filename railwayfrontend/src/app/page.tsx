735984fb989840c21848439c2964ea15b175390e
Exif <lacednyquil@gmail.com>
2024-12-02 16:46:28 -0800

    fixed backend link

--- railwayfrontend/src/app/page.tsx
+++ railwayfrontend/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [todo, setTodo] = useState<string>('');
   const [todosList, setTodosList] = useState<string[]>([]);
 
-  const BACKEND_URL = "backend-production-9639.up.railway.app";
+  const BACKEND_URL = "https://backend-production-9639.up.railway.app";
 
   useEffect(() => {
     fetchTodos();
